54a17d3bfe47a02ef4fd896330203eae78e76ee5
Charles <chall70101@gmail.com>
2021-12-09 11:30:41 -0700

    commit tree 5de0529028b1e195cadbcdede3b6a8433e7225a8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,19 @@ class String
 
   def sentence?
     
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   # binding.pry
+    self.scan(/[.?!]+(?=\s|\z)/).count
   end
 end
